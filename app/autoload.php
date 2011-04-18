adc4690ed8d832d97c5417f42e81c5a8048bd28f
alexandresalome <alexandre.salome@gmail.com>
2011-04-18 12:03:59 +0200

    Fix loading and vendors

--- app/autoload.php
+++ app/autoload.php
@@ -12,7 +12,8 @@ use Symfony\Component\ClassLoader\UniversalClassLoader;
 
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
-    'Symfony'                        => array(__DIR__.'/../vendor/symfony/src', __DIR__.'/../vendor/bundles'),
+    'Symfony'                        => __DIR__.'/../vendor/symfony/src',
+    'Doctrine\\Common\\DataFixtures' => __DIR__.'/../vendor/doctrine-fixtures/lib',
     'Doctrine\\Common'               => __DIR__.'/../vendor/doctrine-common/lib',
     'Doctrine\\DBAL'                 => __DIR__.'/../vendor/doctrine-dbal/lib',
     'Doctrine'                       => __DIR__.'/../vendor/doctrine/lib',
