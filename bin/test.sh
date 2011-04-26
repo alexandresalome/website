21db30fec2ef4748b2dae754d24061661459ff95
alexandresalome <alexandre.salome@gmail.com>
2011-04-26 22:46:58 +0200

    Add check of parameters.ini in test script + return code is the PHPUnit return code

--- bin/test.sh
+++ bin/test.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 rm -Rf app/cache/* app/logs/*
+
+if [ ! -f "app/config/parameters.ini" ]; then
+  cp app/config/parameters.ini-dist app/config/parameters.ini
+fi
+
 ./app/console doctrine:database:drop --force --env=test
 ./app/console doctrine:database:create --env=test
 ./app/console doctrine:schema:update --force --env=test
 ./app/console doctrine:data:load --env=test
+
 phpunit -c app/
+exit $?
