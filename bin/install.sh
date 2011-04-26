2ecd4dcbaec8e8ca9e8d8cb19c92ebf1bd4c4847
alexandresalome <alexandre.salome@gmail.com>
2011-04-26 07:55:57 +0200

    Change directory before installing project + check configuration

--- bin/install.sh
+++ bin/install.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+DIR=`php -r "echo dirname(dirname(realpath('$0')));"`
+cd "$DIR"
+
 rm -Rf app/cache/* app/logs/*
+
+if [ ! -f "app/config/parameters.ini" ]; then
+  cp app/config/parameters.ini-dist app/config/parameters.ini
+fi
+
 ./app/console doctrine:database:drop --force
 ./app/console doctrine:database:create
 ./app/console doctrine:schema:update --force
