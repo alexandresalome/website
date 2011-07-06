d973ccc8167ca6c25a2217111669ed38de214f9e
alexandresalome <alexandre.salome@gmail.com>
2011-07-06 23:41:58 +0200

    Tests are back

--- src/Alom/Website/MainBundle/Test/Client.php
+++ src/Alom/Website/MainBundle/Test/Client.php
@@ -11,6 +11,46 @@ use Symfony\Bundle\FrameworkBundle\Client as BaseClient;
  */
 class Client extends BaseClient
 {
+    static protected $connection;
+
+    protected $requested;
+
+    /**
+     * Makes a request.
+     *
+     * @param Request $request A Request instance
+     *
+     * @return Response A Response instance
+     */
+    protected function doRequest($request)
+    {
+        if ($this->requested) {
+            $this->kernel->shutdown();
+            $this->kernel->boot();
+        }
+
+        $this->injectConnection();
+        $this->requested = true;
+
+        return $this->kernel->handle($request);
+    }
+
+    protected function injectConnection()
+    {
+        if (null === self::$connection) {
+            self::$connection = $this->getContainer()->get('doctrine.dbal.default_connection');
+        } else {
+            if (! $this->requested) {
+                self::$connection->rollback();
+            }
+            $this->getContainer()->set('doctrine.dbal.default_connection', self::$connection);
+        }
+
+        if (! $this->requested) {
+            self::$connection->beginTransaction();
+        }
+    }
+
     /**
      * Connect to Alom website
      *
--- src/Alom/Website/MainBundle/Test/WebTestCase.php
+++ src/Alom/Website/MainBundle/Test/WebTestCase.php
@@ -6,19 +6,10 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as BaseWebTestCase;
 
 abstract class WebTestCase extends BaseWebTestCase
 {
-    static protected $connection;
-
     static protected function createClient(array $options = array(), array $server = array())
     {
         $client = parent::createClient($options, $server);
         $client->getContainer()->get('session')->clear();
-        if (null === self::$connection) {
-            self::$connection = $client->getContainer()->get('doctrine.dbal.default_connection');
-        } else {
-            self::$connection->rollback();
-            $client->getContainer()->set('doctrine.dbal.default_connection', self::$connection);
-        }
-        self::$connection->beginTransaction();
 
         return $client;
     }
