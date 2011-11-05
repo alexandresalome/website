188490e40163517909ee768b1142ee7889d06235
alexandresalome <alexandre.salome@gmail.com>
2011-11-05 18:34:15 +0100

    [Tests] Remove an old hack

--- src/Alom/Website/MainBundle/Test/WebTestCase.php
+++ src/Alom/Website/MainBundle/Test/WebTestCase.php
@@ -6,14 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase as BaseWebTestCase;
 
 abstract class WebTestCase extends BaseWebTestCase
 {
-    static protected function createClient(array $options = array(), array $server = array())
-    {
-        $client = parent::createClient($options, $server);
-        $client->getContainer()->get('session')->clear();
-
-        return $client;
-    }
-
     public function assertTextSimilar($left, $right, $message = '') {
         $left  = preg_replace('/\w+/', ' ', trim($left));
         $right = preg_replace('/\w+/', ' ', trim($right));
